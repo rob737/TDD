609eb4a060b9a407ba321d5aa80b93dbf88ca18a
rob737 <robinsrivastava737@gmail.com>
2023-05-24 08:12:33 +0530

    24-05-23 : Junit handson

--- src/main/java/com/example/demo/DemoUtils.java
+++ src/main/java/com/example/demo/DemoUtils.java
@@ -4,4 +4,8 @@ public class DemoUtils {
     public int add(int a, int b){
         return a+b;
     }
+
+    public Object getReference(){
+        return null;
+    }
 }
--- src/test/java/com/example/demo/DemoUtilsTest.java
+++ src/test/java/com/example/demo/DemoUtilsTest.java
@@ -1,10 +1,23 @@
 package com.example.demo;
 
+//import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 /*
 * Explore auto creation of test cases provided by intellij.
+* Note: We can use import static instead of import statements above to directly
+* use assertion methods of Assertions class.
+*
+* We can also use other annotations based on the use case.
+* @BeforeEach -- executes the annotated method before executing each test
+* @AfterEach -- as the name suggests
+* @BeforeAll - Executed only once before any test is executed
+* @AfterAll - Executed only once after all tests are executed
+*
+* Note: method annotated with @BeforeAll and @AfterAll must be static.
 * */
 public class DemoUtilsTest {
 
@@ -17,6 +30,13 @@ public class DemoUtilsTest {
         // Execute
         int actualResult = demoUtils.add(a,b);
         // Verify
-        Assertions.assertEquals(expectedResult,actualResult,"TDD is the way to code!");
+        assertEquals(expectedResult,actualResult,"TDD is the way to code!");
+    }
+
+    @Test
+    void testNull(){
+        DemoUtils demoUtils = new DemoUtils();
+        Object ref = demoUtils.getReference();
+        assertNull(ref);
     }
 }
