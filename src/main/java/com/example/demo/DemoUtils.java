72c739eee34039c6d34ecad111bfad0ae45a53c2
rob737 <robinsrivastava737@gmail.com>
2023-05-29 08:32:37 +0530

    29-05-23 : Junit annotations

--- src/main/java/com/example/demo/DemoUtils.java
+++ src/main/java/com/example/demo/DemoUtils.java
@@ -1,11 +1,34 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DemoUtils {
+    private String stringOne = "Robin";
+    private String stringTwo = "Robin";
+    private String[] strArray = {"Robin","Ruchika","Foster"};
+    private List<String> listArray = List.of("Robin","Ruchika","Foster");
+
     public int add(int a, int b){
         return a+b;
     }
 
     public Object getReference(){
         return null;
+    }
+
+    public String getFirstReference(){
+        return stringOne;
+    }
+    public String getSecondReference(){
+        return stringTwo;
+    }
+
+    public String[] getFamilyMembers(){
+        return strArray;
+    }
+
+    public List<String> getFamilyMembersList(){
+        return listArray;
     }
 }
--- src/test/java/com/example/demo/DemoUtilsTest.java
+++ src/test/java/com/example/demo/DemoUtilsTest.java
@@ -3,8 +3,9 @@ package com.example.demo;
 //import org.junit.jupiter.api.Assertions;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import java.util.List;
 
 /*
 * Explore auto creation of test cases provided by intellij.
@@ -17,16 +18,54 @@ import org.junit.jupiter.api.Test;
 * @BeforeAll - Executed only once before any test is executed
 * @AfterAll - Executed only once after all tests are executed
 *
+* @DisplayName - Annotation that can be used to give names to our test cases,
+* helpful for documentation and knowledge sharing.
 * Note: method annotated with @BeforeAll and @AfterAll must be static.
+*
+* Note: we can also hint junit to generate names for us using generators.
+* @DisplayNameGeneration(DisplayNameGenerator.Simple.class) can be decorated
+* at class level and each @DisplayName annotation can be removed to get the work done.
+*
+* assertSame(): Test for equal reference
+* assertNotSame(): Test for unequal reference
+*
+* assertTrue() and assertFalse(): As the name suggests.
+*
+* ------- Important --------------
+* assertArrayEquals() : asserts that both object arrays are deeply equal.
+* assertIterableEquals() : asserts that both object iterables are deeply equal. (can be used to test
+* any class that implements Iterable interface.)
+* assertLinesMatch() : asserts that both lists are equal
 * */
+
+@DisplayNameGeneration(DisplayNameGenerator.Standard.class)
 public class DemoUtilsTest {
+    DemoUtils demoUtils;
+
+    /*@BeforeAll
+    static void generateMessage(){
+        System.out.println("Before all demonstration ...");
+    }*/
+
+    @BeforeEach
+    void demonstrateBeforeEach(){
+//        System.out.println("Before each test case ...");
+        demoUtils = new DemoUtils();
+    }
+
+    /*@AfterEach
+    //@DisplayName("After each demonstration ...")
+    void demonstrateAfterEach(){
+        System.out.println("After each test case ...");
+    }*/
 
     @Test
+    @DisplayName("Addition of two numbers.")
     void testAddition(){
         int a=2,b=4;
         int expectedResult = a+b;
         // Setup
-        DemoUtils demoUtils = new DemoUtils();
+      //  DemoUtils demoUtils = new DemoUtils();
         // Execute
         int actualResult = demoUtils.add(a,b);
         // Verify
@@ -34,9 +73,39 @@ public class DemoUtilsTest {
     }
 
     @Test
+//    @DisplayName("Testing null cases behaviour.")
     void testNull(){
-        DemoUtils demoUtils = new DemoUtils();
+
         Object ref = demoUtils.getReference();
         assertNull(ref);
     }
+
+    @Test
+    void checkReference(){
+        String firstRef = demoUtils.getFirstReference();
+        String secondRef = demoUtils.getSecondReference();
+        String testRef = "RobinS";
+
+        // It is coming as same because of JVM optimisation as String value is same.
+        assertSame(firstRef,secondRef);
+        assertNotSame(testRef,secondRef);
+    }
+
+    @Test
+    void testArrays(){
+        String[] arr = {"Robin","Ruchika","Foster"};
+        assertArrayEquals(arr,demoUtils.getFamilyMembers());
+    }
+
+    @Test
+    void testIterable(){
+        List<String> expectedList = List.of("Robin","Ruchika","Foster");
+        assertIterableEquals(expectedList,demoUtils.getFamilyMembersList());
+        assertLinesMatch(expectedList,demoUtils.getFamilyMembersList());
+    }
+
+   /* @AfterAll
+    static void demoAfterAll(){
+        System.out.println("After all demonstration ...");
+    }*/
 }
