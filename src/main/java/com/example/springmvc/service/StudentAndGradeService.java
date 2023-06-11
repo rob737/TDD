1c16bd5c3b5f8794ba300ad2c168ce3e8fe1ba7b
rob737 <robinsrivastava737@gmail.com>
2023-06-11 09:23:49 +0530

    11-06-23 : DAO TDD

--- src/main/java/com/example/springmvc/service/StudentAndGradeService.java
+++ src/main/java/com/example/springmvc/service/StudentAndGradeService.java
@@ -6,16 +6,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @Transactional
 public class StudentAndGradeService {
 
     @Autowired
-    private StudentDao studentDao;
+    StudentDao studentDao;
 
     public void createStudent(String firstName, String lastName, String email){
         CollegeStudent collegeStudent = new CollegeStudent(firstName,lastName,email);
-        collegeStudent.setId(0);
+        collegeStudent.setId(2);
         studentDao.save(collegeStudent);
+    }
+
+    public boolean checkIfStudentIsNull(int id) {
+        Optional<CollegeStudent> student = studentDao.findById(id);
+        return student.isEmpty();
+    }
+
+    public void deleteStudentById(int id) {
+        studentDao.deleteById(id);
     }
 }
--- src/test/java/com/example/springmvc/StudentAndGradeServiceTest.java
+++ src/test/java/com/example/springmvc/StudentAndGradeServiceTest.java
@@ -3,26 +3,64 @@ package com.example.springmvc;
 import com.example.springmvc.models.CollegeStudent;
 import com.example.springmvc.repository.StudentDao;
 import com.example.springmvc.service.StudentAndGradeService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+/*
+* Since, we have included H2 as a dependency in pom,
+* spring boot will auto configure H2 as our primary db for test cases.
+* So, save method that we have used below is storing data in H2 DB
+* and corresponding fetch is fetching it from H2 DB.
+* */
 @PropertySource("/application.properties")
 @SpringBootTest
 public class StudentAndGradeServiceTest {
 
     @Autowired
-    private StudentAndGradeService studentService;
+    JdbcTemplate jdbcTemplate;
     @Autowired
-    private StudentDao studentDao;
+    StudentAndGradeService studentService;
+    @Autowired
+    StudentDao studentDao;
+
+    @BeforeEach
+    void setupDb(){
+        jdbcTemplate.execute("insert into student(ID, firstname, lastname, email_address)" +
+                " values (0, 'Robin', 'Srivastava', 'robin@srivastav.com')");
+    }
 
     @Test
     void createStudentService(){
         studentService.createStudent("Robin","Srivastava","robin@srivastava.com");
         CollegeStudent collegeStudent = studentDao.findByEmailAddress("robin@srivastava.com");
         assertEquals("robin@srivastava.com",collegeStudent.getEmailAddress());
+    }
+
+    @Test
+    void checkIsStudentNullCheck(){
+        assertFalse(studentService.checkIfStudentIsNull(0));
+        assertTrue(studentService.checkIfStudentIsNull(1));
+    }
+
+    @Test
+    void deleteStudentById(){
+        Optional<CollegeStudent> student = studentDao.findById(0);
+        assertTrue(student.isPresent());
+        studentService.deleteStudentById(0);
+    }
+
+    @AfterEach
+    void tearDownDb(){
+        jdbcTemplate.execute("delete from student");
     }
 }
